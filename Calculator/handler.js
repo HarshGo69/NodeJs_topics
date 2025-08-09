78c82e091bb67f6b1ad49d2fc55b17813dfb454f
Harsh Gosai <harshgosai23m@gmail.com>
2025-08-09 13:40:34 +0530

    added conditions

--- Calculator/handler.js
+++ Calculator/handler.js
@@ -1,18 +1,35 @@
 //To manage incomming request from the server
+const { sumRequestHandler } = require('./sum');
 const requestHandler = (req, res) => {
   console.log(req.url , req.method);
-  if(req.url === './'){
-    res.setheader('Content-type','text/html');
+  if(req.url === '/'){
+    res.setHeader('Content-type','text/html');
     res.write(` <html>
-      <head><title>Practise Set</head></title>
+      <head><title>Practise Set</title></head>
       <body><h1>welcome to calcultor</h1></body>
-      <a href="/calcultor">go to calcultor</a>
+      <a href="/calculator">go to calcultor</a>
       </html>`);
       return res.end();
   }
-  res.setheader('Content-type','text/html');
+  else if(req.url.toLowerCase() === '/calculator'){
+    res.setHeader('Content-type','text/html');
     res.write(` <html>
-      <head><title>Practise Set</head></title>
+      <head><title>Practise Set</title></head>
+      <body><h1>Here is the calculator</h1></body>
+      <form action="/calculate-result" method="POST"> 
+      <input type="text" placeholder="First num" name="first" />
+      <input type="text" placeholder="secound num" name="secound" />
+      <input type="submit" value="Sum">
+      </form>
+      </html>`);
+      return res.end();
+  }
+  else if(req.url.toLowerCase() === '/calculate-result' && req.method === 'POST'){
+    return sumRequestHandler(req, res);
+  }
+  res.setHeader('Content-type','text/html');
+    res.write(` <html>
+      <head><title>Practise Set</title></head>
       <body><h1>404 Error - Page does not exist</h1></body>
       <a href="/">Go to Home</a>
       </html>`);
