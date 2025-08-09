1234dc70c063a92f60ce64a28224e77a7a48649f
Harsh Gosai <harshgosai23m@gmail.com>
2025-08-09 13:41:02 +0530

    Calculted the sum of number

---
+++ Calculator/sum.js
@@ -0,0 +1,19 @@
+const sumRequestHandler = (req, res) => {
+  console.log('In sumRequestHandler' ,req.url);
+  const body =[];
+  req.on('data', chunk => body.push(chunk));
+  req.on('end', () => {
+    const bodyStr = Buffer.concat(body).toString();
+    const params = new URLSearchParams(bodyStr);
+    const bodyObj = Object.fromEntries(params);
+    const result = Number(bodyObj.first) + Number(bodyObj.secound);
+    console.log(result);
+    res.setHeader('Content-type','text/html');
+res.write(` <html>
+      <head><title>Practise Set</title></head>
+      <body><h1>Your result is ${result}</h1></body> </html>`);
+return res.end();
+  })
+};
+
+exports.sumRequestHandler = sumRequestHandler;
