4b1ba94caa15542592e3a4e729c8bd19514e846b
Harsh Gosai <harshgosai23m@gmail.com>
2025-08-09 13:40:01 +0530

    Correct error of GET/

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,8 +1,8 @@
 //Created a server 
 const http = require("http");   //imported modules
-const requestHandler = require('./handler'); //imported from handler.js
+const { requestHandler } = require('./handler'); //imported from handler.js
 const server = http.createServer(requestHandler);
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 })
