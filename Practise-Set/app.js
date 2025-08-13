ae6c9dc6bc4da65e67a5229a054b64903f2af945
Harsh Gosai <harshgosai23m@gmail.com>
2025-08-13 14:04:26 +0530

    Handled 404 error

--- Practise-Set/app.js
+++ Practise-Set/app.js
@@ -10,7 +10,11 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded());
 app.use(userRouter);
-app.use(hostRouter);
+app.use("/host", hostRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>404 error</h1>`); // Error Handling
+})
 
 const PORT = 3000; 
 app.listen(PORT , () => {
